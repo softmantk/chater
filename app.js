1ab04e767c33915c1cd3314843d5554fb5b3cbb7
nikhilcm <rose12nikhl@gmail.com>
2017-06-27 22:30:16 +0530

    fixed db

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+require('dotenv').config();
 var passport = require('passport');
 require('./models/models');
 var index = require('./routes/index');
 var authenticate = require('./routes/authenticate')(passport);
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/test-chirp');
-mongoose.connect('mongodb://admin:admin@ds139082.mlab.com:39082/chatter-db', function (err) {
-    if(err)  throw err
+//var uri = "mongodb://admin:admin@ds139082.mlab.com:39082/chatter-db"
+var uri = "mongodb://"+process.env.DB_USER + ":" + process.env.DB_PASS + "@" + process.env.DB_HOST + "/" + process.env.DB_DBNAME
+mongoose.connect(uri, function (err) {
+    if (err)  throw err
     console.log("Connected");
 });
+
 var app = express();
 
 // view engine setup
@@ -23,33 +27,28 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(session({
-  secret: 'keyboard cat'
+    secret: 'keyboard cat'
 }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 var initPassport = require('./passport-init');
 initPassport(passport);
-
 //app.use();
 app.use('/', index);
 app.use('/auth', authenticate);
 
 
-
-
-
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -57,7 +56,7 @@ if (app.get('env') === 'development') {
         });
     });
 }
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
