7a842bbc4190f19aae87e92c300dc28667b8cfaa
Nikhil CM <nikhil.spericorn.com>
2017-06-27 18:30:52 +0530

    ds

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+require('./models/models');
+var index = require('./routes/index');
+var authenticate = require('./routes/authenticate')(passport);
+var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/test-chirp');
+mongoose.connect('mongodb://localhost:27017/chat', function (err) {
+    if(err)  throw err
+    console.log("Connected")
+});
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(session({
+  secret: 'keyboard cat'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+//app.use();
+app.use('/', index);
+app.use('/auth', authenticate);
+
+
+
+
+
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+module.exports = app;
